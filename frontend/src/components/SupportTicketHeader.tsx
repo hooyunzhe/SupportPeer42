15fe1e2e9ad2c570fc0f115e90062b7ef70f7aad
ghostyghast <antoinethebestgamer@gmail.com>
2023-10-08 14:34:28 +0800

    feat:still working on ticket, adding assigned bocal slot

--- frontend/src/components/SupportTicketHeader.tsx
+++ frontend/src/components/SupportTicketHeader.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Flex, Title } from '@mantine/core';
+import { Avatar, Badge, Flex, Title } from '@mantine/core';
 
 interface SupportTicketHeaderProps {
   title: string;
@@ -10,10 +10,16 @@ export default function SupportTicketHeader({
   status,
 }: SupportTicketHeaderProps) {
   return (
-    <Flex bg='black' w='100%' h='25%' justify={'center'} align='center'>
-      <Title order={3} c='white'>
-        {title}
-      </Title>
+    <Flex bg='black' w='100%' h='25%' justify={'space-between'} align='center'>
+      <Flex ml='10'>
+        <Title order={3} c='white'>
+          {title}
+        </Title>
+      </Flex>
+      <Flex align='center' justify='space-between' w='60' mr='10'>
+        <Badge color='green'></Badge>
+        <Avatar src='elon.png' size='20'></Avatar>
+      </Flex>
     </Flex>
   );
 }
